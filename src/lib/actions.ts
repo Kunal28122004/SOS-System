55add9e8a222e65826692feef3a2d866bd818ed8
Kunal Chauhan <kunalchauhan40058@gmail.com>
2025-12-14 16:45:49 +0000

    Try fixing this error: `Runtime Error: FIREBASE FATAL ERROR: Can't deter

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,3 +1,4 @@
+
 "use server";
 
 import { revalidatePath } from "next/cache";
@@ -14,19 +15,22 @@ import {
   collection 
 } from "firebase/firestore";
 import { getDatabase, ref, set, push, get } from "firebase/database";
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { initializeApp, getApps, getApp, type FirebaseOptions } from "firebase/app";
 
 import type { User, Admin, EmergencyContact } from "./definitions";
 
 // This is a server-side only Firebase instance.
-const firebaseConfig = {
+const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
 };
 
-const serverApp = !getApps().length ? initializeApp(firebaseConfig, "server") : getApp("server");
+const serverApp = !getApps().some(app => app.name === 'server') 
+  ? initializeApp(firebaseConfig, "server") 
+  : getApp("server");
+  
 const db = getFirestore(serverApp);
 const realtimeDB = getDatabase(serverApp);
 
